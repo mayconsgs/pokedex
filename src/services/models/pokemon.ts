804ae3be8b896ecb8344be8235433517cd100142
Maycon Santos <maycon.s.santos44@gmail.com>
2022-05-20 19:40:37 -0300

    feat: define pokemon types

---
+++ src/services/models/pokemon.ts
@@ -0,0 +1,94 @@
+import { TypeNames } from "./types";
+
+export type Ability = {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+};
+
+type PokemonMoveVersion = {
+  level_learned_at: number;
+  move_learn_method: {
+    name: string;
+    url: string;
+  };
+  version_group: {
+    name: string;
+    url: string;
+  };
+};
+
+export type PokemonMove = {
+  move: {
+    name: string;
+    url: string;
+  };
+  version_group_details: PokemonMoveVersion[];
+};
+
+export type PokemonSprites = {
+  front_default: string;
+  front_female: string | null;
+  front_shiny: string;
+  front_shiny_female: string | null;
+  other: {
+    dream_world: {
+      front_default: string;
+      front_female: string | null;
+    };
+    home: {
+      front_default: string;
+      front_female: string | null;
+      front_shiny: string;
+      front_shiny_female: string | null;
+    };
+    "official-artwork": {
+      front_default: string;
+    };
+  };
+};
+
+type StatNames =
+  | "hp"
+  | "attack"
+  | "defense"
+  | "special-attack"
+  | "special-defense"
+  | "speed"
+  | "accuracy"
+  | "evasion";
+
+export type PokemonStat = {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: StatNames;
+    url: string;
+  };
+};
+
+export type PokemonType = {
+  slot: 1;
+  type: {
+    name: TypeNames;
+    url: string;
+  };
+};
+
+export type Pokemon = {
+  id: string;
+  name: string;
+  base_experience: string;
+  height: number;
+  is_default: boolean;
+  order: number;
+  weight: number;
+  abilities: Ability[];
+  moves: PokemonMove[];
+  sprites: PokemonSprites;
+  stats: PokemonStat;
+  types: PokemonType[];
+};
